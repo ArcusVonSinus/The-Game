b2d48c58d20d06f183c72dd28da8342f85330698
Ondrej Bouchala <ondrej.bouchala@gmail.com>
2013-11-25 14:56:13 +0100

    Komentare

--- The Game/The Game/The Game/AnimatedSprite.cs
+++ The Game/The Game/The Game/AnimatedSprite.cs
@@ -58,12 +58,12 @@ namespace The_Game
     {
         public Texture2D Texture;
         public Texture2D TextureHead;
-        public Texture2D TextureFront;
-        public int Rows,RowsHead;
+        public Texture2D TextureFront; //napriklad ruka, to, co ma byt nad hlavou
+        public int Rows,RowsHead; //pocet radku textury
         public int Columns,ColumnsHead;
-        private int currentFrame,currentFrameHead;
-        private int totalFrames,totalFramesHead;
-        float relSize, relPosX, relPosY;
+        private int currentFrame,currentFrameHead; 
+        private int totalFrames,totalFramesHead; 
+        float relSize, relPosX, relPosY; //to udava, jak velka ma byt hlava a kde ma byt umistena - manualne jsem to vyladil pro ten 12-ti sten aby to vypadalo dobre, pro jine hlavy je to treba napocitat znovu, predava se to jako parametr constructoru
         public int width
         {
             get { return Texture.Width / Columns; }
@@ -87,7 +87,7 @@ namespace The_Game
         }
         public AnimatedSpriteHead(Texture2D[] texture, int rows, int columns, int rowsHead, int columnsHead, Vector3 pos)
         {
-           AnimatedSpriteHeadCons (texture, rows, columns, rowsHead, columnsHead, pos.X, pos.Y, pos.Z);           
+           AnimatedSpriteHeadCons (texture, rows, columns, rowsHead, columnsHead, pos.X, pos.Y, pos.Z);           //ted netusim, proc jsem to rozdelil, ale asi jem pro to mel duvod...
         }
         void AnimatedSpriteHeadCons(Texture2D[] texture,int rows, int columns,int rowsHead, int columnsHead,float relSize, float relPosX,float relPosY)
         {
@@ -134,27 +134,26 @@ namespace The_Game
         }
         public void Draw(SpriteBatch spriteBatch, Vector2 location, int width)
         {
-            int width2 = Texture.Width / Columns;
+            int width2 = Texture.Width / Columns;     //pocet pixelu jednoho dilku tela postavicky
             int height = Texture.Height / Rows;
-            int width2Head = TextureHead.Width / ColumnsHead;
+            int width2Head = TextureHead.Width / ColumnsHead;   //pocet pixelu 1 dilku hlavy
             int heightHead = TextureHead.Height / RowsHead;
-            int row = (int)((float)currentFrame / (float)Columns);
+            int row = (int)((float)currentFrame / (float)Columns);          //ve kterem radku/sloupci te velke textury prave jsem na zaklade currentFrame, popripade currentFrameHead
             int rowHead = (int)((float)currentFrameHead / (float)ColumnsHead);
-            int column = currentFrame % Columns;
+            int column = currentFrame % Columns;   
             int columnHead = currentFrameHead % ColumnsHead;
 
-            Rectangle sourceRectangle = new Rectangle(width2 * column, height * row, width2, height);
-            Rectangle destinationRectangle = new Rectangle((int)location.X, (int)location.Y, width, (width * height) / width2);
+            Rectangle sourceRectangle = new Rectangle(width2 * column, height * row, width2, height);   //odkud se to bude vybirat (z te velke textury
+            Rectangle destinationRectangle = new Rectangle((int)location.X, (int)location.Y, width, (width * height) / width2);  // kam se to bude vykreslovat (t.j. rozmery po prepocitani) (location dostane jako parametr)
 
-            int widthHead=(int) (relSize*width);
-            Rectangle sourceRectangleHead = new Rectangle(width2Head * columnHead, heightHead * rowHead, width2Head, heightHead);
-            Rectangle destinationRectangleHead = new Rectangle((int)location.X + (int)(relPosX * width), (int)location.Y + (int)(relPosY * width * height/ width2), widthHead, (int)(widthHead * heightHead) / width2Head);
-            //Rectangle destinationRectangleHead = new Rectangle((int)location.X + (int)(relPosX * width), (int)location.Y + (int)(relPosY * width * height / width2), widthHead, 50);
-            spriteBatch.Draw(Texture, destinationRectangle, sourceRectangle, Color.White);
+            int widthHead=(int) (relSize*width);   //prepocitana velikost hlavy na zaklade velikosti tela
+            Rectangle sourceRectangleHead = new Rectangle(width2Head * columnHead, heightHead * rowHead, width2Head, heightHead);    //ktery frame hlavy se vybere
+            Rectangle destinationRectangleHead = new Rectangle((int)location.X + (int)(relPosX * width), (int)location.Y + (int)(relPosY * width * height/ width2), widthHead, (int)(widthHead * heightHead) / width2Head); //kam se hlava vlozi            
+            spriteBatch.Draw(Texture, destinationRectangle, sourceRectangle, Color.White); //vykresli telo
             if (TextureHead != null)
-            spriteBatch.Draw(TextureHead, destinationRectangleHead, sourceRectangleHead, Color.White);
+            spriteBatch.Draw(TextureHead, destinationRectangleHead, sourceRectangleHead, Color.White); //pokud je hlava, vykresli hlavu
             if(TextureFront!=null)
-                spriteBatch.Draw(TextureFront, destinationRectangle, sourceRectangle, Color.White);
+                spriteBatch.Draw(TextureFront, destinationRectangle, sourceRectangle, Color.White); //pokud je ruka zvlast, vykresli ruku (napr. kdyz leti, tak musi byt hlava nad telem a ruka nad hlavou
         }
     }
 }
